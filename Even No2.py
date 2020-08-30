9d1f1075a469eef548981848911e0569135569e0
sagar15101995 <58854948+sagar15101995@users.noreply.github.com>
2020-08-31 01:24:07 +0530

    Even Odd in While

---
+++ Even No2.py
@@ -0,0 +1,15 @@
+n=int(input("Enter a Number"))
+i=1
+while(i<=n):
+    if i%2==0:
+     print(i)
+    i=i+1
+   
+   
+   
+   
+   n=int(input("Enter a Number"))
+   i=2
+   while(i<=n):
+        print(i)
+        i=i+2
